d0a057d74f9e768c6ea1d01c7cba6ff4a750972d
Bart Botta <00003b@gmail.com>
2009-01-08 04:54:57 -0600

    add some more info from tamarin opcode list, mark missing opcodes #s

--- asm/opcodes.lisp
+++ asm/opcodes.lisp
@@ -30,6 +30,8 @@ for that here and return count of extra args"
   (:dxnslate   ()                      #x07  1 0 0 0 0 +set-dxns+)
   (:kill       ((local-index u30))     #x08  0 0)
   (:label      ()                      #x09  0 0)
+  ;; #x0a
+  ;; #x0b
   (:if-nlt     ((offset ofs24))          #x0c  2 0)
   (:if-nle     ((offset ofs24))          #x0d  2 0)
   (:if-ngt     ((offset ofs24))          #x0e  2 0)
@@ -69,8 +71,11 @@ for that here and return count of extra args"
   (:push-scope     ()                                             #x30  1 0 0 1)
   (:push-namespace ((namespace namespace-q30))                        #x31  0 1)
   (:has-next-2     ((object-local-reg u30) (index-local-reg u30)) #x32  0 1 0 0 (max object-local-reg index-local-reg))
+  ;; #x33
+  ;; #x34
   ;; #x35-#x3e: flash 10/alchemy instrs
   ;; see http://ncannasse.fr/blog/adobe_alchemy
+  ;; (are these backwards? tamarin has li*/lf* then si*/sf*
   (:store-int-8     ()   #x35 2 0) ;; stack=value,addr -> .
   (:store-int-16    ()   #x36 2 0) ;; stack=value,addr -> .
   (:store-int-32    ()   #x37 2 0) ;; stack=value,addr -> .
@@ -81,7 +86,7 @@ for that here and return count of extra args"
   (:load-int-32     ()   #x3c 1 1) ;; stack=addr -> value
   (:load-float-32   ()   #x3d 1 1) ;; stack=addr -> value
   (:load-float-64   ()   #x3e 1 1) ;; stack=addr -> value
-
+  ;; #x3f
   (:new-function    ((method-index method-u30))                    #x40  0 1)
   (:call            ((arg-count u30))                       #x41  (+ 2 arg-count) 1)
   (:construct       ((arg-count u30))                       #x42  (1+ arg-count) 1)
@@ -102,14 +107,16 @@ for that here and return count of extra args"
   (:sign-extend-1to32     ()   #x50 1 1) ;; stack=value,value
   (:sign-extend-8to32     ()   #x51 1 1) ;; stack=value,value
   (:sign-extend-16to32    ()   #x52 1 1) ;; stack=value,value
-
-
+  ;; (:apply-type (?) #x53 1 1) ??
+  ;; #x54
   (:new-object           ((arg-count u32))       #x55  (* 2 arg-count) 1)
   (:new-array            ((arg-count u30))       #x56  arg-count 1)
   (:new-activation       ()                      #x57  0 1 0 0 0 +need-activation+)
   (:new-class            ((class-index u30))     #x58  1 1)  ;; 2->1 ? also see docs about scope stuff
   (:get-descendants      ((multiname multiname-q30)) #x59  (1+ (runtime-name-count multiname)) 1)
   (:new-catch            ((exception-index u30)) #x5a  0 1)
+  ;; #x5b
+  ;; #x5c
   (:find-property-strict ((multiname multiname-q30)) #x5d  (runtime-name-count multiname) 1)
   (:find-property        ((multiname multiname-q30)) #x5e  (runtime-name-count multiname) 1)
   (:find-def             ((string string-u30))    #x5f  0 1) ;; ??
@@ -120,8 +127,11 @@ for that here and return count of extra args"
   (:get-global-scope ()                      #x64  0 1)
   (:get-scope-object ((scope-index u8))      #x65  0 1)
   (:get-property     ((multiname multiname-q30)) #x66  (1+ (runtime-name-count multiname)) 1)
+  ;;(:get-outer-scope (?) #x67 0 1) ??
   (:init-property    ((multiname multiname-q30)) #x68  (+ 2 (runtime-name-count multiname)) 0)
+  ;; #x69
   (:delete-property  ((multiname multiname-q30)) #x6a (1+ (runtime-name-count multiname)) 1)
+  ;; #x6b
   (:get-slot         ((slot-index u30))      #x6c  1 1)
   (:set-slot         ((slot-index u30))      #x6d  2 0)
   (:get-global-slot  ((slot-index u30))      #x6e  0 1) ;; deprecated?
@@ -135,6 +145,13 @@ for that here and return count of extra args"
   (:convert-boolean  () #x76  1 1)
   (:convert-object   () #x77  1 1)
   (:check-filter     () #x78  1 1)
+  ;; #x79
+  ;; #x7a
+  ;; #x7b
+  ;; #x7c
+  ;; #x7d
+  ;; #x7e
+  ;; #x7f
   (:coerce        ((multiname multiname-q30)) #x80  1 1)
   (:coerce-b      ()                      #x81  1 1) ;; deprecated?
   (:coerce-any    ()                      #x82  1 1)
@@ -145,6 +162,12 @@ for that here and return count of extra args"
   (:as-type-late  ()                      #x87  2 1)
   (:coerce-u      ()                      #x88  1 1) ;; deprecated?
   (:coerce-object ()                      #x89  1 1)
+  ;; #x8a
+  ;; #x8b
+  ;; #x8c
+  ;; #x8d
+  ;; #x8e
+  ;; #x8f
   (:negate    ()                  #x90  1 1)
   (:increment ()                  #x91  1 1)
   (:inc-local ((local-index u30)) #x92  0 0 0 0 local-index)
@@ -155,6 +178,10 @@ for that here and return count of extra args"
   (:bit-not   ()                  #x97  1 1)
   ;; (:concat ?                   #x9a ? ?)
   ;; (:add_d  ?                   #x9b ? ?)
+  ;; #x9b
+  ;; #x9e
+  ;; #x9d
+  ;; #x9f
   (:add             () #xa0  2 1)
   (:subtract        () #xa1  2 1)
   (:multiply        () #xa2  2 1)
@@ -178,6 +205,17 @@ for that here and return count of extra args"
   (:is-type        ((multiname multiname-q30)) #xb2  1 1)
   (:is-type-late   ()                      #xb3  2 1)
   (:in             ()                      #xb4  2 1)
+  ;; #xb5
+  ;; #xb6
+  ;; #xb7
+  ;; #xb8
+  ;; #xb9
+  ;; #xba
+  ;; #xbb
+  ;; #xbc
+  ;; #xbd
+  ;; #xbe
+  ;; #xbf
   (:increment-i ()                  #xc0  1 1)
   (:decrement-i ()                  #xc1  1 1)
   (:inc-local-i ((local-index u30)) #xc2  0 0 0 0 local-index)
@@ -186,6 +224,14 @@ for that here and return count of extra args"
   (:add-i       ()                  #xc5  2 1)
   (:subtract-i  ()                  #xc6  2 1)
   (:multiply-i  ()                  #xc7  2 1)
+  ;; #xc8
+  ;; #xc9
+  ;; #xca
+  ;; #xcb
+  ;; #xcc
+  ;; #xcd
+  ;; #xce
+  ;; #xcf
   (:get-local-0 () #xd0  0 1 0 0 0)
   (:get-local-1 () #xd1  0 1 0 0 1)
   (:get-local-2 () #xd2  0 1 0 0 2)
@@ -194,6 +240,29 @@ for that here and return count of extra args"
   (:set-local-1 () #xd5  1 0 0 0 1)
   (:set-local-2 () #xd6  1 0 0 0 2)
   (:set-local-3 () #xd7  1 0 0 0 3)
+  ;; #xd8
+  ;; #xd9
+  ;; #xda
+  ;; #xdb
+  ;; #xdc
+  ;; #xdd
+  ;; #xde
+  ;; #xdf
+  ;; #xe0
+  ;; #xe1
+  ;; #xe2
+  ;; #xe3
+  ;; #xe4
+  ;; #xe5
+  ;; #xe6
+  ;; #xe7
+  ;; #xe8
+  ;; #xe9
+  ;; #xea
+  ;; #xeb
+  ;; #xec
+  ;; #xed
+  ;; (:abs-jump () #xee 0 0) ?
   (:debug ((debug-type u8) (string string-u30) (register u8) (extra u30)) #xef  0 0)
   (:debug-line ((line-number u30))  #xf0  0 0)
   (:debug-file ((string string-u30)) #xf1  0 0)
