9e83a6ed455b0ca4b2fcaa2e19c706bfc126d8cf
Bart Botta <00003b@gmail.com>
2009-10-22 02:57:13 -0500

    missing file from previous commit

---
+++ lib/bootstrap.lisp
@@ -0,0 +1,176 @@
+(in-package #:avm2-compiler)
+
+;;; random stuff needed for compiling other stuff, but not in CL:
+;;; as well as some of the more primitive CL bits
+
+(let ((*symbol-table* *cl-symbol-table*))
+
+  #+old
+
+  (%defmacro defmacro (name args &body body)
+    (let ((form (gensym))
+          (environment (gensym)))
+      (add-swf-macro-function
+       name
+       (coerce `(lambda (,form ,environment)
+                  (declare (ignore ,environment))
+                  (destructuring-bind ,args (cdr ,form)
+                    ,@body)) 'function)))
+    nil)
+
+  ;; defmacro
+  (add-swf-macro-function
+   'defmacro
+   (lambda (form environment)
+     (declare (ignore environment))
+     (destructuring-bind (name args &body body) (cdr form)
+       (let ((bform (gensym))
+             (benvironment (gensym)))
+         (add-swf-macro-function
+          name
+          (coerce `(lambda (,bform ,benvironment)
+                     (declare (ignore ,benvironment))
+                     (destructuring-bind ,args (cdr ,bform)
+                       ,@body)) 'function))))
+     nil))
+
+  (c3* (gensym)
+
+   (defmacro %typep (object type)
+     `(%asm
+       (:@ ,object)
+       (:get-lex ,(or (swf-name (find-swf-class type)) type))
+       (:is-type-late)))
+
+
+    (defmacro %new- (class &rest args)
+      (let ((name (typecase class
+                        (symbol
+                         (let ((c (find-swf-class class)))
+                           (assert c) ;; fixme: better error reporting
+                           (swf-name c)))
+                        (t class))))
+            `(%asm (:find-property-strict ,name)
+                   ,@(loop for i in args
+                        collect `(:@ ,i))
+                   (:comment "%new-")
+                   (:construct-prop ,name ,(length args)))))
+
+
+
+      (defmacro %apply (function this-arg rest-array)
+        `(flash:apply ,function ,this-arg ,rest-array))
+
+      (defmacro %funcall (function this-arg &rest rest)
+        `(flash:call ,function ,this-arg ,@rest))
+
+
+
+      (defmacro %setf-1 (place value)
+        `(%setf ,place ,value))
+
+      #++
+      (defmacro defun-setf (name args  body)
+        (%swf-defun name args (list
+                               (loop for i in body
+                                     if (and (consp i) (eql (car i) 'cl))
+                                     collect (cadr i)
+                                     else
+                                     collect i #+nil(list 'quote i)))
+                    :method t
+                    :class-name 'setf-namespace-type
+                    :class-static t)
+        nil)
+
+
+    (defmacro defclass-swf (class-name (&optional (superclass-name 'flash:object))
+                            (&rest slot-specifiers)
+                            &rest class-options)
+      (let ((properties nil)
+            (static-properties nil)
+            (forms nil)
+            (constructor-sym (gensym (format nil "~a-CTOR-" class-name)))
+            (constructor (cdr (assoc :constructor class-options)))
+            (super-args (cdr (assoc :super-args class-options)))
+            (fake-accessors (second (assoc :fake-accessors class-options))))
+        (format t "slot-spec = ~s~%" slot-specifiers)
+        ;; todo: class options
+        ;;  (:swf-flags :sealed <bool> :final <bool> :interface <bool> ...?)
+        ;;  :metaclass? :documentation :default-initargs?
+        (map 'nil
+             (lambda (spec)
+               (destructuring-bind (name &key
+                                         ;; same? as cl
+                                         allocation
+                                         ;; similar to CL, but strict, and currently unused
+                                         type
+                                         ;; currently unsupported CL stuff
+                                         ;; reader writer accessor initarg initform
+                                         ;; unused, but might as well allow it
+                                         documentation) (if (listp spec) spec (list spec))
+                 (declare (ignore type documentation))
+                                        ;(format t "slot ~s = ~s ~s ~s ~s~%" name allocation inline-reader inline-writer inline-accessor)
+                 #++(when fake-accessors
+                   (let ((a (gensym)))
+                     (push `(defmacro ,name (,a)
+                              `(slot-value ,,a ,',name))
+                           forms)))
+                 (when fake-accessors
+                   (add-swf-accessor name name))
+                 (if (eq allocation :class)
+                     (push name static-properties)
+                     (push name properties))))
+             slot-specifiers)
+
+        ;; fixme: this should use eval-when instead of doing things at
+        ;; macroexpansion time, once that is supported
+        (setf (gethash class-name (classes *symbol-table*))
+              (add-swf-class
+               class-name
+               (avm2-asm::symbol-to-qname-list class-name)
+               :ns "???" ;; fixme: what goes here?
+               :extends superclass-name
+               :properties properties
+               :class-properties static-properties
+               :functions nil       ;; to be added separately
+               :class-functions nil ;; to be added separately
+               ;; fixme: should we define a default constructor with
+               ;; initform/initarg parsing, and optionally call out to
+               ;; a user-defined constructor?
+               :constructor constructor-sym))
+        (loop for p in properties
+           do (add-swf-property p p))
+        (loop for p in static-properties
+           do (add-swf-class-property p p))
+                                        ;(format t "constructor = ~%")
+        (print
+         `(progn
+            ,@(nreverse forms)
+            (%named-lambda ,constructor-sym
+                  (:no-auto-return t :no-auto-scope t :anonymous t)
+                ,(car constructor)      ; lambda list
+              (%asm
+               (:get-local-0)
+               (:push-scope)
+               (:get-local-0)
+               ,@(loop for i in super-args
+                    collect `(:@ ,i))
+               (:construct-super ,(length super-args))
+               (:%activation-record)
+               (:push-null))
+              (block nil
+                ,@(cdr constructor))
+              (%asm (:return-void)))
+            ))))
+
+
+
+    (defclass-swf cons-type (flash:object)
+      (%car %cdr)
+      (:fake-accessors t)
+      (:constructor (a b)
+        (setf (%car this) a)
+        (setf (%cdr this) b)))
+
+))
+
