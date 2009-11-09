518aea263a7b16bc7e19f918f188f79662cf3205
Bart Botta <00003b@gmail.com>
2009-11-08 18:50:34 -0600

    convert roots sample to new compiler/libs

--- test/roots-new.lisp
+++ test/roots-new.lisp
@@ -1,129 +1,134 @@
 (in-package :avm2-compiler)
-;;; sample from old version. works but needs more refactoring
 
 (with-open-file (s "/tmp/roots5.swf"
                    :direction :output
-                              :element-type '(unsigned-byte 8)
-                              :if-exists :supersede)
-  (with-compilation-to-stream s ("frame1" `((0 "testClass")))
+                   :element-type '(unsigned-byte 8)
+                   :if-exists :supersede)
 
-    (def-swf-class :test-class "test-class" %flash.display::sprite (text)
-                   (()
-                    (ftrace (s+ "constructor - " this))
-                    (main this)))
+  (3b-swf:write-swf
+   s
+   (append
+    (list
+     (3b-swf:file-attributes :has-metadata t :as3 t :use-network t
+                             :use-gpu t :direct-blit t)
+     (3b-swf:script-limits 60 1000)
+     (3b-swf:background-color #x869ca7)
+     (3b-swf:frame-label "frame1"))
 
-    (c3* :top-level
-      (defun random-range (a b)
-        (+ a (floor (random (- b a)))))
+    (compile-abc-tag (((nil :test-class))
+                      :inherit (avm2-compiler::*cl-symbol-table*
+                                fu::%*fu-symbol-table*))
 
-      (defun radians (a)
-        (/ (* a %flash:+pi+) 180.0))
+      (c3* :top-level
+        (defclass-swf :test-class (flash:flash.display.sprite)
+          (text
+           canvas
+           (%app :allocation :class ))
+          (:fake-accessors t)
+          (:constructor ()
+            (main this)))
 
-      (defun i255 (a)
-        (max (min (floor (* a 256)) 255) 0))
+        (defun app ()
+          (%get-property-static :test-class %app))
 
-      (defun rgb (r g b)
-        (+ (* (i255 r) 65536) (* (i255 g) 256) (i255 b)))
+        (defun random-range (a b)
+          (+ a (floor (random (- b a)))))
 
-      (defun rgba (r g b a)
-        (+ (* (i255 a) 65536 256) (rgb r g b)))
+        (defun main (arg)
+          (let ((canvas (fu:display (%new- flash:flash.display.sprite)
+                                    :parent arg))
+                (foo (fu:text-field :width 200
+                                    :auto-size "left"
+                                    :text-color (fu:rgb 0.2 0.9 0.2)
+                                    :word-wrap t
+                                    :background t
+                                    :background-color (fu:rgba 0.1 0.1 0.1 0.1)
+                                    :text "..."
+                                    :parent arg)))
+            (%set-property-static :test-class %app arg)
+            ;(setf (%app arg) arg)
+            (setf (canvas arg) canvas)
+            (setf (text arg) foo)
+            (frame nil)
+            (flash:add-event-listener canvas "click" #'frame)))
 
-      (defun main (arg)
-        (let ((foo (%new- %flash.text:text-Field))
-              (canvas (%new- %flash.display:sprite)))
-          (setf (%flash.display:width foo) 200)
-          (setf (%flash.text:auto-size foo) "left")
-          (setf (%flash.text:text-color foo) (rgb 0.2 0.9 0.2 ))
-          (setf (%flash.text:word-wrap foo) t)
-          (setf (%flash.text:background foo) t)
-          (setf (%flash.text:background-color foo) (rgba 0.1 0.1 0.1 0.1))
-          (let ((str "abc..."))
-            (setf (%flash.text:text foo)
-                  (s+ str (%flash:to-string (vector 1 2 3)))))
-          (%flash.display:add-child arg canvas)
-          (%flash.display:add-child arg foo)
-          (%set-property- this :tc arg)
-          (%set-property- this :canvas canvas)
-          (setf (text arg) foo)
-          (frame nil)
-          (%flash.display:add-event-listener canvas "click" (function frame))))
+        (defun frame (evt)
+          (let* ((canvas (canvas (app)))
+                 (gfx (flash:.graphics canvas))
+                 (matrix (%new- flash:flash.geom.matrix)))
 
-      (defmacro with-fill (gfx (color alpha &key line-style) &body body)
-        `(progn
-           ,@(when line-style
-                   `((%flash.display:line-style ,gfx ,@line-style)))
-           (%flash.display:begin-fill ,gfx ,color ,alpha)
-           ,@body
-           (%flash.display:end-fill ,gfx)))
+            (setf (flash:.opaque-background canvas) #x0d0f00)
+            (flash:clear gfx)
+            (fu:with-fill gfx (#x202600  0.5)
+                          (flash:draw-rect gfx 0 0 400 300 ))
+            (flash:create-gradient-box matrix 400 300 0 0 0)
+            (flash:begin-gradient-fill gfx "radial"
+                                       (vector #x202600 #x0d0f00) ;; colors
+                                       (vector 1 1)  ;; alpha
+                                       (vector 0 255) ;; ratios
+                                       matrix)
+            (flash:draw-rect gfx 0 0 400 300 )
+            (flash:end-fill gfx)
+            (ftrace "click")
+            (time
+             (setf (flash:.text (text (app)))
+                   (+ "==" (root canvas 200 150 (random 360) 7 1.0 0.005 0))))
+            (ftrace "frame done")))
 
-      (defun frame (evt)
-        (let* ((canvas (%get-property- this :canvas))
-               (gfx (slot-value canvas '%flash.display:graphics ))
-               (matrix (%new- %flash.geom:Matrix)))
+        (defun root (canvas x y angle depth alpha decay count)
+          (let* ((s (* depth 0.5))
+                 (w (* s 6.0))
+                 (line-size (* s 0.5))
+                 (gfx (flash:.graphics canvas))
+                 (times (* depth (random-range 10 20))))
+            (when (< times 0)
+              (ftrace (s+ " root: times = " times))
+              (return-from root 0))
+            (dotimes (i times)
+              (let* ((v (/ depth 5.0))
+                     (color (fu:rgb  (- 0.8 (* v 0.25))
+                                     0.8
+                                     (- 0.8 v))))
+                (setf alpha (max 0.0 (- alpha (* i decay))))
+                ;; stop if alpha gets below 1/256 or so
+                (when (> alpha 0.004)
+                  (setf angle (+ angle (random-range -60 60)))
+                  (let ((dx (+ x (* (cos (fu:radians angle)) w)))
+                        (dy (+ y (* (sin (fu:radians angle)) w))))
 
-          (setf (%flash.display:opaque-background canvas) #x0d0f00)
-          (%flash.display:clear gfx)
-          (with-fill gfx (#x202600  0.5)
-            (%flash.display:draw-rect gfx 0 0 400 300 ))
-          (%flash.geom:create-gradient-box matrix
-                                           400 300 0 0 0)
-          (%flash.display:begin-gradient-fill
-           gfx "radial"
-           (vector #x202600 #x0d0f00) ;; colors
-           (vector 1 1)               ;; alpha
-           (vector 0 255)             ;; ratios
-           matrix)
-          (%flash.display:draw-rect gfx 0 0 400 300 )
-          (%flash.display:end-fill gfx)
-          (ftrace "click")
-          (setf (%flash.text:text (%get-property- (%get-property- this :tc) text))
-                (+ "==" (root canvas 200 150 (random 360) 7 1.0 0.005 0)))
-          (ftrace "frame done")))
+                    ;; drop shadow
+                    (fu:with-fill gfx (0 (* alpha 0.6)
+                                         :line-style (flash:+number.nan+ 0 alpha))
+                                  (flash:draw-circle gfx
+                                                     (+ x s 1) (1- (+ y s))
+                                                     (/ w 3)))
 
-      (defun root (canvas x y angle depth alpha decay count)
-        (let* ((s (* depth 0.5))
-               (w (* s 6.0))
-               (line-size (* s 0.5))
-               (gfx (slot-value canvas '%flash.display:graphics ))
-               (times (* depth (random-range 10 20))))
-          (when (< times 0)
-            (ftrace (s+ " root: times = " times))
-            (return-from root 0))
-          (dotimes (i times)
-            (let* ((v (/ depth 5.0))
-                   (color (rgb  (- 0.8 (* v 0.25))
-                                0.8
-                                (- 0.8 v))))
-              (setf alpha (max 0.0 (- alpha (* i decay))))
-              ;; stop if alpha gets below 1/256 or so
-              (when (> alpha 0.004)
-                (setf angle (+ angle (random-range -60 60)))
-                (let ((dx (+ x (* (cos (radians angle)) w)))
-                      (dy (+ y (* (sin (radians angle)) w))))
+                    ;; line segment to next position:
+                    (fu:with-fill gfx (color (* alpha 0.6)
+                                             :line-style (line-size color alpha))
+                                  (flash:move-to gfx x y)
+                                  (flash:line-to gfx dx dy))
 
-                  ;; drop shadow
-                  (with-fill gfx (0 (* alpha 0.6) :line-style (%flash:+nan+ 0 alpha))
-                    (%flash.display:draw-circle gfx (+ x s 1) (1- (+ y s)) (/ w 3)))
+                    ;; filled circle
+                    (fu:with-fill gfx (color (* alpha 0.5)
+                                             :line-style ((* 0.5 line-size)
+                                                          color alpha))
+                                  (flash:draw-circle gfx x y (/ w 4)))
+                    (incf count)
+                    (when (and (> depth 0) (> (random 1.0) 0.85))
+                      (setf count
+                            (root canvas x y (+ angle (random-range -60 60))
+                                  (1- depth) alpha decay (1+ count))))
+                    (setf x dx)
+                    (setf y dy)))))
 
-                  ;; line segment to next position:
-                  (with-fill gfx (color (* alpha 0.6)
-                                        :line-style (line-size color alpha))
-                    (%flash.display:move-to gfx x y)
-                    (%flash.display:line-to gfx dx dy))
+            (when (and (> depth 0) (> (random 1.0) 0.7))
+              (setf count (root canvas x y angle (1- depth) alpha decay (1+ count)))))
+          count)))
+    (list
+     (3b-swf:show-frame)))
 
-                  ;; filled circle
-                  (with-fill gfx (color (* alpha 0.5)
-                                        :line-style ((* 0.5 line-size)
-                                                     color alpha))
-                    (%flash.display:draw-circle gfx x y (/ w 4)))
-                  (incf count)
-                  (when (and (> depth 0) (> (random 1.0) 0.85))
-                    (setf count
-                          (root canvas x y (+ angle (random-range -60 60))
-                                (1- depth) alpha decay (1+ count))))
-                  (setf x dx)
-                  (setf y dy)))))
-
-          (when (and (> depth 0) (> (random 1.0) 0.7))
-            (setf count (root canvas x y angle (1- depth) alpha decay (1+ count)))))
-        count))))
+   :x-twips 400
+   :y-twips 300
+   :frame-rate 30
+   :flash-version 9))
